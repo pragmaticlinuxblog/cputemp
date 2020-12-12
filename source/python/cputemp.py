8f63e7c6003a305575a31a0d4ce66a1f98c49e47
PragmaticLinux <info@pragmaticlinux.com>
2020-12-12 16:48:06 +0100

    Added file exists check to function get_cpu_temp()

--- source/python/cputemp.py
+++ source/python/cputemp.py
@@ -18,15 +18,16 @@ def get_cpu_temp():
     result = 0.0
     # The first line in this file holds the CPU temperature as an integer times 1000.
     # Read the first line and remove the newline character at the end of the string.
-    with open('/sys/class/thermal/thermal_zone0/temp') as f:
-        line = f.readline().strip()
-    # Test if the string is an integer as expected.
-    if line.isdigit():
-        # Convert the string with the CPU temperature to a float in degrees Celsius.
-        result = float(line) / 1000
+    if os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
+        with open('/sys/class/thermal/thermal_zone0/temp') as f:
+            line = f.readline().strip()
+        # Test if the string is an integer as expected.
+        if line.isdigit():
+            # Convert the string with the CPU temperature to a float in degrees Celsius.
+            result = float(line) / 1000
     # Give the result back to the caller.
     return result
-
+    
 
 if __name__ == "__main__":
     main()
